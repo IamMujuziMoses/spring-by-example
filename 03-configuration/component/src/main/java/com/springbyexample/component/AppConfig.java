293afd8bdb49ac04bb0918ef726095cf2facd63d
IamMujuziMoses <mujuzimoses@gmail.com>
2026-07-29 22:48:18 +0300

    (feature): Added @Component example demonstrating automatic bean discovery

---
+++ 03-configuration/component/src/main/java/com/springbyexample/component/AppConfig.java
@@ -0,0 +1,12 @@
+package com.springbyexample.component;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Mujuzi Moses
+ */
+@Configuration
+@ComponentScan("com.springbyexample.component")
+public class AppConfig {
+}
---
+++ 03-configuration/component/src/test/java/com/springbyexample/component/GreetingServiceTest.java
@@ -0,0 +1,24 @@
+package com.springbyexample.component;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @author Mujuzi Moses
+ */
+public class GreetingServiceTest {
+
+    @Test
+    void shouldDiscoverComponentAutomatically() {
+
+        try (var context = new AnnotationConfigApplicationContext(AppConfig.class)) {
+            GreetingService greetingService = context.getBean(GreetingService.class);
+
+            assertNotNull(greetingService);
+            assertEquals("Hello from GreetingService!", greetingService.greet());
+        }
+    }
+}
