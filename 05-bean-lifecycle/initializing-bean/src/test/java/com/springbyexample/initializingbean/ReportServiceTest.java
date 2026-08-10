436e70c81fc0f941ac59f8ab16c6f8bcb56861c1
IamMujuziMoses <mujuzimoses@gmail.com>
2026-08-10 15:28:12 +0300

    (feature): Added InitializingBean example demonstrating bean initialization lifecycle

---
+++ 05-bean-lifecycle/initializing-bean/src/test/java/com/springbyexample/initializingbean/ReportServiceTest.java
@@ -0,0 +1,22 @@
+package com.springbyexample.initializingbean;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @author Mujuzi Moses
+ */
+public class ReportServiceTest {
+
+    @Test
+    void shouldInvokeAfterPropertiesSetDuringBeanInitialization() {
+        try (var context = new AnnotationConfigApplicationContext(AppConfig.class)) {
+
+            ReportService reportService = context.getBean(ReportService.class);
+
+            assertTrue(reportService.isInitialized());
+        }
+    }
+}
