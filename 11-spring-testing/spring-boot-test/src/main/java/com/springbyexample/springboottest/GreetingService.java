765233b4503038d8ff5a7d41ffd2a9ecd0d11674
IamMujuziMoses <mujuzimoses@gmail.com>
2026-09-04 16:49:54 +0300

    (feature): Added @SpringBootTest example demonstrating Spring Boot application context testing

---
+++ 11-spring-testing/spring-boot-test/src/main/java/com/springbyexample/springboottest/GreetingService.java
@@ -0,0 +1,14 @@
+package com.springbyexample.springboottest;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Mujuzi Moses
+ */
+@Service
+public class GreetingService {
+
+    public String greet(String name) {
+        return "Hello, " + name + "!";
+    }
+}
